fb1638a404385ec2dcd75a68e81b2dc1f6d886c2
Jahon <donyorovjahon@gmail.com>
2023-03-18 14:09:26 +0000

    Header component

--- src/pages/companets/header/header.tsx
+++ src/pages/companets/header/header.tsx
@@ -1,8 +1,30 @@
 import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import {AiOutlineSearch, AiOutlineUser} from 'react-icons/ai'
+import {BiBellMinus} from 'react-icons/bi'
 
 function Header() {
+  // scroll bulganda header rangi uzgarishi uchun
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    // scrol bulsa true bulmasa false bulsin degan function
+    const handleScroll = () => {
+      if(window.scrollY > 0) {
+        setScrolled(true)
+      }else{
+        setScrolled(false)
+      }
+    }
+    // hodisa quwilyapdi scroll bulganda function ishga tushsin
+    window.addEventListener('scroll', handleScroll);
+    // ??
+    // return () => window.removeEventListener('scroll', handleScroll);
+  }, [])
+  
   return (
-   <header>
+    // scrolled true bulsa shu ranga kirsin 
+   <header className={`${scrolled && 'bg-[#e10856] shadow-lg'}`}> 
     <div className="flex items-center space-x-2 md:space-x-10">
       {/* md bu responsive uchun mediumdan kiyin shu qiymatga aylanadi  */}
      <Image src={"./logo.svg"} alt={"logo"} width={56}  height={56} /> 
@@ -15,9 +37,17 @@ function Header() {
       <li className="navLink">Popular</li>
      </ul>
     </div>
-    <div></div>
+
+    <div className="flex items-center space-x-4 text-sm font-light">
+      <AiOutlineSearch className="h-6 w-6 cursor-pointer"/>
+      <p className="hidden lg:inline  cursor-pointer font-semibold">Kids</p>
+      <BiBellMinus className="h-6 w-6 cursor-pointer"/>
+      <Link href={'/accaunt'}>
+        <AiOutlineUser className="h-6 w-6 cursor-pointer"/>
+      </Link>
+    </div>
    </header>
   )
 }
 
-export default Header
+export default Header 
